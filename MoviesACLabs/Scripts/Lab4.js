031f20f8662559bc54e9b6d6241ad355bf67ee2b
alex07pep <peptan_alex@yahoo.com>
2016-04-18 16:51:00 +0300

    lab4
    lab4

---
+++ MoviesACLabs/Scripts/Lab4.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('Lab4', []);
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.addEl = '';
+    $scope.elements = [{ name: "one", status: false }, { name: "two", status: false }, { name: "three", status: false }];
+    $scope.addElement = function () {
+        $scope.elements.push({name: $scope.addEl, status: false});
+        //$scope.elements.name =''
+        //$scope.elements.status=false;
+    }
+    $scope.removeElement = function (x) {
+        $scope.elements.splice(x, 1);
+    }
+    //$scope.status = false;
+    $scope.checkboxModel = {
+        value1: true,
+    };
+
+
+
+
+});
+
+
