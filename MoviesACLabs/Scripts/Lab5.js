fc68120ff444d7f6f5a55b653c781e7a5f602ff1
alex07pep <peptan_alex@yahoo.com>
2016-04-25 16:39:08 +0300

    lab 5
    cont angularJS

---
+++ MoviesACLabs/Scripts/Lab5.js
@@ -0,0 +1,56 @@
+﻿var app = angular.module('Lab5', []);
+
+app.service("ActorsService", function ($http) {
+    this.getActors = function () {
+        return $http({
+            method: "GET",
+            url: "/api/Actors"
+        });
+    }
+    this.postActors = function () {
+        return $http({
+            method: "POST",
+            url: "/api/Actors"
+        });    
+    }
+    this.putActors = function () {
+        return $http({
+            method: "PUT",
+            url: "/api/Actors"
+        });
+    }
+
+});
+
+app.controller('ActorsCtrl', function ($scope, ActorsService) {
+    $scope.actorsList = null;
+    ActorsService.getActors().then(function (dataResponse) {
+     
+        $scope.actorsList = dataResponse.data;
+    });
+    $scope.UpdateData = function () {
+        var data = {
+            Name: $scope.Name,
+            DateofBirth: $scope.DateofBirth
+        };
+        $scope.actorsList.push($scope.data);
+       
+    };
+
+
+
+
+
+});
+
+//app.controller('AddCtrl', function ($scope, ActorsService) {
+   
+   // ActorsService.postActors().then(function (data) {
+
+        //$scope.dataResponse.data=data ;
+   // });
+
+//});
+
+
+
